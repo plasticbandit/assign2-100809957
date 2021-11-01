82beca5b4696f58911193eab62ef59f36e8e3a6f
Nicola <nicola.mcfadden@dcmail.ca>
2021-11-01 14:07:02 -0400

    Added parent theme comment header

--- themes/owlpedia-child/functions.php
+++ themes/owlpedia-child/functions.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
 
 function owlpediachild_enqueue_scripts() {
     wp_enqueue_style(
